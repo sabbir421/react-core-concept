9d28ba1f9d6a473197aa5016a1731583b63551c2
sabbir421 <sabbiralazim421@gmail.com>
2021-09-19 03:55:58 +0600

    component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
+  const persons= [
+    {name:"sabbir",age:"22"},
+    {name:"shehab",age:"23"},
+    {name:"azim",age:"21"},
+    {name:"janina",age:"24"},
+    {name:"janina",age:"24"},
+    {name:"janina",age:"24"},
+  ]
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <Persone name="sabbir" id="182" ></Persone>
+      <Persone name="sabbir" id="182" ></Persone>
+      <Persone name="sabbir" id="182" ></Persone>
+      <Persone name="sabbir" id="182" ></Persone> */}
+        {persons.map(person=><Persone name={person.name} age={person.age} ></Persone>)}
     </div>
   );
 }
+
+
+function Persone (props){
+  return(
+    <div style={{backgroundColor:"blue",padding:"2px", margin:"10px",borderRadius:"10px", color:"white",width:"400px", marginLeft:"400px"}}>
+      <h1>Name : {props.name }</h1>
+     
+      <h1>Age : {props.age }</h1>
+      </div>
+      
+  )
+}
+
+
 
 export default App;
